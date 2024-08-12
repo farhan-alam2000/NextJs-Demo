f7e22a8ad4a0ac5d68eacef714a1eb10f1a2477a
Farhan <alaamm.farhan@gmail.com>
2024-08-12 21:53:39 +0530

    Dashboard homepage

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,16 +11,7 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
@@ -41,6 +32,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+        
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
